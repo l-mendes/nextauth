9923f75423078ad34b8d336d75d75bdf2f33ccee
Lucas Mendes Madruga <lucas.madruga@kymo.it>
2022-06-17 14:23:03 -0300

    Encerramento modulo IV

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,22 +1,23 @@
-import { useContext, useEffect } from 'react';
+import { useContext } from 'react';
+import { Can } from '../components/Can';
 import { AuthContext } from '../contexts/AuthContext';
 import { setupAPIClient } from '../services/api';
 
-import { api } from '../services/apiClient';
 import { withSSRAuth } from '../utils/withSSRAuth';
 
 export default function Dashboard() {
-  const { user } = useContext(AuthContext);
-
-  useEffect(() => {
-    api.get('/me')
-      .then(response => console.log(response.data));
-  }, []);
+  const { user, signOut } = useContext(AuthContext);
 
   return (
-    <div>
+    <>
       <h1>Dashboard: {user?.email}</h1>
-    </div>
+
+      <button onClick={signOut}>Sign out</button>
+
+      <Can permissions={['metrics.list']}>
+        <div>Métricas</div>
+      </Can>
+    </>
   );
 }
 
@@ -24,8 +25,6 @@ export const getServerSideProps = withSSRAuth(async (ctx) => {
   const apiClient = setupAPIClient(ctx);
 
   const response = await apiClient.get('/me');
-
-  console.log(response.data);
 
   return {
     props: {}
